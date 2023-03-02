089cd23c4886f6e0a8f1c156b3c67ddcf67d44ea
Leo A. Wolanski <126403089+LWolanski@users.noreply.github.com>
2023-03-01 23:12:03 -0500

    updatings lensgoal.sol

--- contracts/LensGoal.sol
+++ contracts/LensGoal.sol
@@ -25,8 +25,6 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
     // is currently the 0 address for simplicity, edit later
 
     address communityWallet = address(0);
-    uint256 constant HOURS_24 = 1 days;
-    uint256 constant MINUTES_6 = 60 * 6;
 
     // used to identify whether stake is in ether or erc20
     enum TokenType {
@@ -68,7 +66,8 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
         Stake stake;
         Votes votes;
         AdditionalStake[] additionalstakes;
-        string[] proofs;
+        string preProof;
+        string proof;
     }
 
     struct AdditionalStake {
@@ -100,14 +99,15 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
         bool inEther,
         uint256 tokenAmount,
         address tokenAddress,
-        uint256 timestampEnd
+        uint256 timestampEnd,
+        string memory preProof
     ) external payable {
         if (inEther) {
             // require(msg.value > 0, "msg.value must be greater than 0");
             // why user can stake nothing:
             // so that user can have friends stake as "rewards" and themselves stake nothing
             AdditionalStake[] memory additionalstakes;
-            string[] memory proofs;
+            // *** No need to create goal here (because we're only using it once), we can just do that on line 129
             Goal memory goal = Goal(
                 GoalBasicInfo(
                     msg.sender,
@@ -120,7 +120,8 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
                 defaultEtherStake(),
                 Votes(0, 0),
                 additionalstakes,
-                proofs
+                preProof,
+                ""
             );
             userToGoalIds[msg.sender].push(goalId);
             goalIdToGoal[goalId] = goal;
@@ -155,7 +156,8 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
                 Votes(0, 0),
                 // empty list s
                 additionalstakes,
-                proofs
+                preProof,
+                ""
             );
             // push goalId
             userToGoalIds[msg.sender].push(goalId);
@@ -219,7 +221,7 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
 
     // users can write or link to proof on chain to convince voters to vote positevely
     function writeProofs(
-        /** input of strings to write */ string[] memory _proofs,
+        /** input of strings to write */ string memory proof,
         uint256 _goalId
     ) external {
         // check for user to be goal initiator
@@ -227,14 +229,12 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
             goalIdToGoal[_goalId].info.user == msg.sender,
             "not goal creator"
         );
-        // iterate through each proof and append to proofs list
-        for (uint256 i; i < goalId; i++) {
-            goalIdToGoal[_goalId].proofs.push(_proofs[i]);
-        }
+        // update proof
+        goalIdToGoal[_goalId].proof = proof;
     }
 
     // get info of goal (for front end)
-    function getBasicInfo(
+    function getGoalBasicInfo(
         uint256 _goalId
     ) public view returns (GoalBasicInfo memory) {
         return goalIdToGoal[_goalId].info;
@@ -261,6 +261,7 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
     modifier windowOpen(uint256 startTimestamp) {
         require(
             block.timestamp > startTimestamp &&
+                // voting window is one day long, starts at deadline and ends at deadline + 1 days
                 block.timestamp < startTimestamp + 1 days
         );
         _;
@@ -268,24 +269,31 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
 
     // allows stakers to withdraw stake so that they don't purposely vote negatively to get it back
     function withdrawStake(uint256 _stakeId) external {
+        AdditionalStake memory stake = stakeIdToStake[_stakeId];
         // identity check
-        require(stakeIdToStake[_stakeId].staker == msg.sender, "not staker");
+        require(stake.staker == msg.sender, "not staker");
         // safety check
-        require(
-            stakeIdToStake[_stakeId].withdrawn == false,
-            "stake already withdrawn"
-        );
+        require(stake.withdrawn == false, "stake already withdrawn");
         // if stake is in ether, send ether back to msg.sender and set withdrawn to true
-        if (stakeIdToStake[_stakeId].stake.tokenType == TokenType.ETHER) {
-            payable(msg.sender).transfer(stakeIdToStake[_stakeId].stake.amount);
+        if (stake.stake.tokenType == TokenType.ETHER) {
             stakeIdToStake[_stakeId].withdrawn = true;
+            payable(msg.sender).transfer(stake.stake.amount);
         } else {
-            IERC20(stakeIdToStake[_stakeId].stake.tokenAddress).transfer(
-                msg.sender,
-                stakeIdToStake[_stakeId].stake.amount
-            );
             stakeIdToStake[_stakeId].withdrawn = true;
+            IERC20(stake.stake.tokenAddress).transfer(
+                msg.sender,
+                stake.stake.amount
+            );
         }
+    }
+
+    function votingWindowClosedAndStatusIsPending(
+        uint256 _goalId
+    ) internal view returns (bool) {
+        Goal memory goal = goalIdToGoal[_goalId];
+        return
+            block.timestamp > goal.info.deadline + 1 days &&
+            goal.info.status == Status.PENDING;
     }
 
     // Chainlink view function. If returns true, Chainlink will run state-changing performUpkeep() function
@@ -298,10 +306,7 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
         returns (bool upkeepNeeded, bytes memory /* performData */)
     {
         for (uint256 i; i < goalId; i++) {
-            if (
-                block.timestamp > goalIdToGoal[i].info.deadline + 1 days &&
-                goalIdToGoal[i].info.status == Status.PENDING
-            ) {
+            if (votingWindowClosedAndStatusIsPending(i)) {
                 return (true, bytes("LensGoal"));
             }
         }
@@ -314,26 +319,18 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
         for (uint256 i; i < goalId; i++) {
             // define goal var
             // check if voting window has closed and Status has not been set to pending
-            // if status has not been set to pending, that means that the voting window has just closed
-            if (
-                block.timestamp > goalIdToGoal[i].info.deadline + 1 days &&
-                goalIdToGoal[i].info.status == Status.PENDING
-            ) {
+            // if status is pending, that means that the voting window has just closed
+            if (votingWindowClosedAndStatusIsPending(i)) {
                 // get result of votes
                 bool accomplishedGoal = evaluateVotes(i);
                 // if voted true, transfer stakes to user and update status
+                Goal memory goal = goalIdToGoal[i];
                 if (accomplishedGoal) {
-                    transferStakes(
-                        accomplishedGoal,
-                        goalIdToGoal[i].info.goalId
-                    );
                     goalIdToGoal[i].info.status = Status.VOTED_TRUE;
+                    transferStakes(accomplishedGoal, goal.info.goalId);
                 } else {
-                    transferStakes(
-                        accomplishedGoal,
-                        goalIdToGoal[i].info.goalId
-                    );
                     goalIdToGoal[i].info.status = Status.VOTED_FALSE;
+                    transferStakes(accomplishedGoal, goal.info.goalId);
                 }
             }
         }
@@ -343,21 +340,10 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
     function evaluateVotes(uint256 _goalId) internal view returns (bool) {
         Votes memory _votes = goalIdToGoal[_goalId].votes;
         // if 0 votes, send funds back to user
-        if (_goalId == 0) {
+        if (_votes.yes == 0 && _votes.no == 0) {
             return true;
         }
-        // if more yes than no, answer is true
-        if (_votes.yes > _votes.no) {
-            return true;
-        }
-        // if more no than yes, answer is false
-        else if (_votes.no > _votes.yes) {
-            return false;
-        }
-        // if there is an equal amount of yes and no votes, answer is true
-        else {
-            return true;
-        }
+        return _votes.yes >= _votes.no;
     }
 
     // function transfers additional stakes (if any) and user stake to user/community wallet
@@ -365,27 +351,17 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
         /* stakes will be transfered to user or to community wallet/back to stakers depending on this bool */ bool userAccomplishedGoal,
         uint256 _goalId
     ) internal {
+        // define goal in memory
+        Goal memory goal = goalIdToGoal[_goalId];
         transferUserStake(userAccomplishedGoal, _goalId);
-        if (goalIdToGoal[_goalId].additionalstakes.length > 0) {
+        if (goal.additionalstakes.length > 0) {
             if (userAccomplishedGoal) {
-                for (
-                    uint256 i;
-                    i < goalIdToGoal[_goalId].additionalstakes.length;
-                    i++
-                ) {
-                    transferStakeToUser(
-                        goalIdToGoal[_goalId].additionalstakes[i].stakeId
-                    );
+                for (uint256 i; i < goal.additionalstakes.length; i++) {
+                    transferStakeToUser(goal.additionalstakes[i].stakeId);
                 }
             } else {
-                for (
-                    uint256 i;
-                    i < goalIdToGoal[_goalId].additionalstakes.length;
-                    i++
-                ) {
-                    transferStakeBackToStaker(
-                        goalIdToGoal[_goalId].additionalstakes[i].stakeId
-                    );
+                for (uint256 i; i < goal.additionalstakes.length; i++) {
+                    transferStakeBackToStaker(goal.additionalstakes[i].stakeId);
                 }
             }
         }
@@ -393,42 +369,43 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
 
     // function transfers stake back to its staker
     function transferStakeBackToStaker(uint256 _stakeId) internal {
+        AdditionalStake memory stake = stakeIdToStake[_stakeId];
         // safety check
-        if (stakeIdToStake[_stakeId].withdrawn == false) {
-            if (stakeIdToStake[_stakeId].stake.tokenType == TokenType.ETHER) {
+        if (stake.withdrawn == false) {
+            if (stake.stake.tokenType == TokenType.ETHER) {
                 // if stake is in ether, transfer stake amount back to staker
-                payable(stakeIdToStake[_stakeId].staker).transfer(
-                    stakeIdToStake[_stakeId].stake.amount
-                );
+                stakeIdToStake[_stakeId].withdrawn = true;
+                payable(stake.staker).transfer(stake.stake.amount);
             } else {
+                stakeIdToStake[_stakeId].withdrawn = true;
                 // if stake is in erc20, transfer tokens back to staker
-                IERC20(stakeIdToStake[_stakeId].stake.tokenAddress).transfer(
-                    stakeIdToStake[_stakeId].staker,
-                    stakeIdToStake[_stakeId].stake.amount
+                IERC20(stake.stake.tokenAddress).transfer(
+                    stake.staker,
+                    stake.stake.amount
                 );
             }
-            // make sure to set withdrawn to true
-            stakeIdToStake[_stakeId].withdrawn = true;
         }
     }
 
     // function transfers stake to user
     function transferStakeToUser(uint256 _stakeId) internal {
         address user = goalIdToGoal[stakeIdToStake[_stakeId].goalId].info.user;
+        // local var to save gas
+        AdditionalStake memory stake = stakeIdToStake[_stakeId];
         // safety check
-        if (stakeIdToStake[_stakeId].withdrawn == false) {
-            if (stakeIdToStake[_stakeId].stake.tokenType == TokenType.ETHER) {
+        if (stake.withdrawn == false) {
+            if (stake.stake.tokenType == TokenType.ETHER) {
+                stakeIdToStake[_stakeId].withdrawn = true;
                 // if stake is in ether, transfer stake amount to user
-                payable(user).transfer(stakeIdToStake[_stakeId].stake.amount);
+                payable(user).transfer(stake.stake.amount);
             } else {
+                stakeIdToStake[_stakeId].withdrawn = true;
                 // transfer tokens to user
-                IERC20(stakeIdToStake[_stakeId].stake.tokenAddress).transfer(
+                IERC20(stake.stake.tokenAddress).transfer(
                     user,
-                    stakeIdToStake[_stakeId].stake.amount
+                    stake.stake.amount
                 );
             }
-            // make sure to set withdrawn to true
-            stakeIdToStake[_stakeId].withdrawn = true;
         }
     }
 
@@ -438,37 +415,32 @@ contract LensGoal is LensGoalHelpers, AutomationCompatibleInterface {
         uint256 _goalId
     ) internal {
         // safety check
-        require(
-            goalIdToGoal[_goalId].info.status == Status.PENDING,
-            "goal complete"
-        );
+        // create goal var to save gas
+        Goal memory goal = goalIdToGoal[_goalId];
+        require(goal.info.status != Status.PENDING, "goal complete");
 
         if (accomplishedGoal) {
             // if stake is in ether, transfer ether back to user
-            if (goalIdToGoal[_goalId].stake.tokenType == TokenType.ETHER) {
-                payable(goalIdToGoal[_goalId].info.user).transfer(
-                    goalIdToGoal[_goalId].stake.amount
-                );
+            if (goal.stake.tokenType == TokenType.ETHER) {
+                payable(goal.info.user).transfer(goal.stake.amount);
             }
             // if stake is in erc20, transfer tokens to user
             else {
-                IERC20(goalIdToGoal[_goalId].stake.tokenAddress).transfer(
-                    goalIdToGoal[_goalId].info.user,
-                    goalIdToGoal[_goalId].stake.amount
+                IERC20(goal.stake.tokenAddress).transfer(
+                    goal.info.user,
+                    goal.stake.amount
                 );
             }
         } else {
             // if stake is in ether, transfer ether to community wallet
-            if (goalIdToGoal[_goalId].stake.tokenType == TokenType.ETHER) {
-                payable(communityWallet).transfer(
-                    goalIdToGoal[_goalId].stake.amount
-                );
+            if (goal.stake.tokenType == TokenType.ETHER) {
+                payable(communityWallet).transfer(goal.stake.amount);
             }
             // if stake is in erc20, transfer tokens to community wallet
             else {
-                IERC20(goalIdToGoal[_goalId].stake.tokenAddress).transfer(
+                IERC20(goal.stake.tokenAddress).transfer(
                     communityWallet,
-                    goalIdToGoal[_goalId].stake.amount
+                    goal.stake.amount
                 );
             }
         }
